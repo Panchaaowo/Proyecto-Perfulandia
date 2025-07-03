26d73e09d857695d49beb32590072c2b89d0a553
Panchaaowo <fr.mirandam@duocuc.cl>
2025-07-02 20:56:03 -0400

    Delete PerfulandiaSpa directory

--- PerfulandiaSpa/Perfulandia/src/main/java/com/PerfulandiaSpa/Perfulandia/Repository/PedidoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.PerfulandiaSpa.Perfulandia.Repository;
-
-import com.PerfulandiaSpa.Perfulandia.Model.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
